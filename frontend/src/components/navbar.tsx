f23d7b84a211ea8b7d40b30f53b068a2c4f34dfb
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2018-05-27 21:39:48 +0200

    Add new post button on the top of the page

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,13 +1,12 @@
 
 import React, { Component } from "react";
-import { connect } from "react-redux";
+import { withRouter } from "react-router";
 import { bindActionCreators } from "redux";
-import { AppState } from "../reducers";
 import Post from "../model/post";
 
-export default class NavbarComponent extends Component<{ title: string }, {}> {
+class NavbarComponent extends Component<any, {}> {
 
-  constructor(props: { title: string }) {
+  constructor(props: any) {
     super(props);
     this.state = {};
   }
@@ -16,9 +15,13 @@ export default class NavbarComponent extends Component<{ title: string }, {}> {
     let btnStyle = {
       margin: 10
     };
+    console.log(this.props);
     return <header>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">{this.props.title}</a>
+        <a className="navbar-brand" href="#">Repeating questions</a>
+        <div style={{ position: "absolute", right: 10 }}>
+          <button className="btn btn-primary" onClick={() => this.props.history.push("/new-post")}>New post</button>
+        </div>
         {/* <div style={{ position: "absolute", right: 10 }}>
           <button className="btn btn-success" style={btnStyle}>Import thread</button>
           <button className="btn btn-danger" style={btnStyle}>Export thread</button>
@@ -27,3 +30,5 @@ export default class NavbarComponent extends Component<{ title: string }, {}> {
     </header>;
   }
 }
+
+export default withRouter(NavbarComponent);
--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -3,11 +3,12 @@ import { Switch, Route, Link, Redirect } from "react-router-dom";
 import ForumComponent from "./components/forum";
 import NavbarComponent from "./components/navbar";
 import PostFormComponent from "./components/post-form";
+
 export default class Root extends Component<{}, {}> {
 
     render() {
         return <div>
-            <NavbarComponent title="Repeating questions" />
+            <NavbarComponent />
             <div className="container">
                 <Switch>
                     <Route exact path="/" component={ForumComponent} />
